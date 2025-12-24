428d32f3b083d6d30eeaeaf58a91dc165a8279b5
Rakesh Joshi <rakeshjoshiamravati@gmail.com>
2025-12-24 18:10:21 +0530

    Create BubbleSort.cpp

---
+++ Sorting/BubbleSort.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    void bubbleSort(vector<int>& arr) {
+        int n = arr.size();
+        bool swapped;
+  
+        for(int i = 0; i<n-1; i++){
+            swapped = false;
+            for(int j = 0; j<n-i-1; j++){
+                if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+         
+            if (!swapped)
+              break;
+        
+    }
+    }
+};
